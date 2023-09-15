d481afaa6a7af41469c9cdcfd06bdcc8a7f8c68f
Rin-o <rino091370@gmail.com>
2023-09-15 22:42:20 +0200

    draft

--- game.js
+++ game.js
@@ -19,6 +19,25 @@ let player = {
     velocityX: playerVelocityX 
 }
 
+//ball
+
+let ballWidth = 10;
+let ballHeight = 10;
+let ballVelocityX = 3; 
+let ballVelocityY = 2; 
+
+let ball = {
+    x : boardWidth/2,
+    y : boardHeight/2,
+    width: ballWidth,
+    height: ballHeight,
+    velocityX: ballVelocityX,
+    velocityY: ballVelocityY
+}
+
+
+
+
 window.onload = function(){
     board = document.getElementById("board");
     board.height = boardHeight;
@@ -43,6 +62,34 @@ function update() {
     //player
     context.fillStyle = "lightgreen";
     context.fillRect(player.x, player.y,player.width, player.height);
+
+    context.fillStyle = "white";
+    ball.x += ball.velocityX;
+    ball.y += ball.velocityY;
+    context.fillRect(ball.x, ball.y, ball.width, ball.height);
+
+    //bounce the ball off player paddle
+    //if (topCollision(ball, player)|| bottomCollision(ball, player)){
+    //    ball.velocityX *= -1;
+    //}
+
+    //else if (leftCollision(ball, player)|| rightCollision(ball, player)){
+      //  ball.velocityX *= -1;
+    //}
+    if (ball.y <= 0){
+        //if ball touches top of canvas
+        ball.velocityY *= -1; //reverse direction
+    }
+    else if (ball.x <= 0 || (ball.x + ball.width >= boardWidth)){
+        //if ball touches left or right of canvas
+        ball.velocityX *= -1; //reverse direction
+    }
+
+    else if (ball.y + ball.height >= boardHeight) {
+        //if ball touches bottom of canvas
+        //context.fillText("Game Over: Press 'Space' to restart", 80, 400);
+        //gameOver = true;
+    }
 }
 
 function outOfBounds(xPosition){
